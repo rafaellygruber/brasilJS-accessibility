f2643c45ca449d0109c4cd0e5481f4252a74e4dd
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2019-10-24 08:37:46 -0300

    funcionando parcialmente

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const taken_usernames = [ 'nicole' ];
+const input = document.getElementById('username');
 
-document.getElementById('username').addEventListener('keyup', function(){
-	function setError(el, msg) {
-		el.parentNode.querySelector('strong').innerHTML = "Error:";
-		el.parentNode.className='error';
-		el.parentNode.querySelector('span').innerHTML = msg;
-	}
-	function setSuccess(el) {
-		el.parentNode.querySelector('strong').innerHTML = "OK:";
-		el.parentNode.className='success';
-		el.parentNode.querySelector('span').innerHTML = "&check;";
-	}
-	var val = this.value;
-	if (val !== "") {
-		if (taken_usernames.indexOf(val.trim())+1) {
-			setError(this, '&cross; Sorry, this username is taken.');
+input.addEventListener('keyup', () => {
+	
+	const inputValue = input.value;
+
+	if (inputValue !== "") {
+		if (taken_usernames.includes(inputValue.trim())) {
+			setError(input, '&cross; Sorry, this username is taken.');
 		} else {
-			setSuccess(this, '&check; You can use this username.');
+			setSuccess(input, '&check; You can use this username.');
 		}
 	} else {
 		document.getElementById('username_feedback').innerHTML = '';
-		document.getElementById('username_feedback')
-			.parentNode.className = '';
+		document.getElementById('username_feedback').parentNode.className = '';
 		document.querySelector('[for="username"] strong').innerHTML = '';
 	}
 });
+
+function setError(el, msg) {
+	el.parentNode.querySelector('strong').innerHTML = "Error:";
+	el.parentNode.className='error';
+	el.parentNode.querySelector('span').innerHTML = msg;
+}
+
+function setSuccess(el) {
+	el.parentNode.querySelector('strong').innerHTML = "OK:";
+	el.parentNode.className='success';
+	el.parentNode.querySelector('span').innerHTML = "&check;";
+}
